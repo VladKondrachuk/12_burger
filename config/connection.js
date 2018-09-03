b02d6d659e23dc38ada5fc6fb3495565bcaa6122
Vladimir Kondrachuk <vladkondrachuk@gmail.com>
2018-09-02 19:55:33 -0600

    heroku part 2

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 // Set up MySQL connection.
 let mysql = require("mysql");
 
-let connection = mysql.createConnection({
-  host: "ec2-50-16-196-57.compute-1.amazonaws.com",
-  port: 5432,
-  user: "vupzjyurobubws",
-  password: "ac89c5ecd826cdf7c475dee7024cbdad6d05c082140f8191244badb8842dd9d8",
-  database: "d6v0si915kc80c",
-  //url: "postgres://vupzjyurobubws:ac89c5ecd826cdf7c475dee7024cbdad6d05c082140f8191244badb8842dd9d8@ec2-50-16-196-57.compute-1.amazonaws.com:5432/d6v0si915kc80c"
-});
+let connection;
 
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+  });
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
