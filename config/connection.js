9170e2c3e418c7bc29808063c20285da63b3da21
Vladimir Kondrachuk <vladkondrachuk@gmail.com>
2018-09-02 19:27:38 -0600

    heroku problems

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ let connection = mysql.createConnection({
   user: "vupzjyurobubws",
   password: "ac89c5ecd826cdf7c475dee7024cbdad6d05c082140f8191244badb8842dd9d8",
   database: "d6v0si915kc80c",
-  url: "postgres://vupzjyurobubws:ac89c5ecd826cdf7c475dee7024cbdad6d05c082140f8191244badb8842dd9d8@ec2-50-16-196-57.compute-1.amazonaws.com:5432/d6v0si915kc80c"
+  //url: "postgres://vupzjyurobubws:ac89c5ecd826cdf7c475dee7024cbdad6d05c082140f8191244badb8842dd9d8@ec2-50-16-196-57.compute-1.amazonaws.com:5432/d6v0si915kc80c"
 });
 
 // Make connection.
